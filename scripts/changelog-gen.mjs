b267d9eb9a425668d23672fdd2e44673ab00f59e
Aadi <goooolok727@gmail.com>
2024-10-29 08:56:33 +0530

    feat: add script for changelog generation

---
+++ scripts/changelog-gen.mjs
@@ -0,0 +1,103 @@
+import { execSync } from 'node:child_process';
+import { appendFileSync, existsSync, readdirSync } from 'node:fs';
+import path from 'node:path';
+
+// https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional#type-enum
+const commitTypes = [
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test',
+  'other',
+];
+
+const CHANGE_SET_DIR = path.resolve(process.cwd(), '.changeset');
+const CHANGE_LOG_MD_REGEX = /^[a-z]+-[a-z]+-[a-z]+\.md$/;
+
+function getChangelogFile() {
+  if (!existsSync(CHANGE_SET_DIR)) {
+    console.error('no .changeset dir found');
+    process.exit(1);
+  }
+
+  const dirContents = readdirSync(CHANGE_SET_DIR, {
+    withFileTypes: true,
+    encoding: 'utf-8',
+  });
+
+  const found = dirContents.find(
+    item => item.isFile() && CHANGE_LOG_MD_REGEX.test(item.name)
+  );
+  if (!found) return null;
+  return path.join(found.parentPath, found.name);
+}
+
+function processLog(log, groups) {
+  const lines = log.split('\n');
+
+  for (const line of lines) {
+    if (line.startsWith('chore: version packages')) return true;
+
+    const match = line.match(/^(\w+)(?:\(.+\))?:/);
+    if (match && commitTypes.includes(match[1])) {
+      groups[match[1]].push(line);
+    } else {
+      groups['other'].push(line);
+    }
+  }
+
+  return false;
+}
+function readGitCommits(chunks = 20) {
+  return new Promise(resolve => {
+    const groupedCommits = commitTypes.reduce((g, type) => {
+      g[type] = [];
+      return g;
+    }, Object.create(null));
+
+    let start = 0;
+    let done = false;
+
+    while (!done) {
+      const log = execSync(
+        `git --no-pager log --pretty=format:%s --skip=${start} -n ${chunks}`,
+        { encoding: 'utf-8' }
+      );
+      done = processLog(log, groupedCommits);
+      start += chunks;
+    }
+
+    resolve(groupedCommits);
+  });
+}
+
+readGitCommits().then(groups => {
+  const markdown = generateMarkdown(groups);
+  const changeSetFile = getChangelogFile();
+  if (!changeSetFile) {
+    console.warn('No changeset file found! Exiting');
+    process.exit(0);
+  }
+  appendFileSync(changeSetFile, markdown, { encoding: 'utf-8' });
+});
+
+function generateMarkdown(groups) {
+  let markdown = '';
+
+  for (const [type, commits] of Object.entries(groups)) {
+    if (commits.length > 0) {
+      markdown += `## ${type.charAt(0).toUpperCase() + type.slice(1)}\n`;
+      markdown += commits.map(msg => `- ${msg}`).join('\n');
+      markdown += '\n';
+    }
+  }
+
+  return markdown;
+}
